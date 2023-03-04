1c6d1eedf39d6be2d0d5bbad2d2cce119222a438
mohammad-ahmadi-r <mamad.dezel14@gmail.com>
2023-03-04 12:56:10 +0000

    repo name changed

--- cdkpipeline/cdkpipeline_stack.py
+++ cdkpipeline/cdkpipeline_stack.py
@@ -3,18 +3,19 @@ from aws_cdk import (
     Stack,
     # aws_sqs as sqs,
 )
+import aws_cdk as cdk
 from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
 from constructs import Construct
 
-class CdkpipelineStack(Stack):
+class CdkpipelineStack(cdk.Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        pipeline =  CodePipeline(self, "Pipeline", 
+        CodePipeline(self, "Pipeline", 
                 pipeline_name="MyPipeline",
                 synth=ShellStep("Synth", 
-                    input=CodePipelineSource.git_hub("OWNER/REPO", "main"),
+                    input=CodePipelineSource.git_hub("mohammad-ahmadi-r/cdk-pipeline", "main"),
                     commands=["npm install -g aws-cdk", 
                         "python -m pip install -r requirements.txt", 
                         "cdk synth"]
