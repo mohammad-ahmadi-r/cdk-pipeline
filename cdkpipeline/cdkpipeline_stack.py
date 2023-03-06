b607315abca511f40de479f5223e06d2880cbff7
mohammad-ahmadi-r <mamad.dezel14@gmail.com>
2023-03-06 07:19:09 +0000

    stages added

--- cdkpipeline/cdkpipeline_stack.py
+++ cdkpipeline/cdkpipeline_stack.py
@@ -5,12 +5,15 @@ import aws_cdk as cdk
 from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
 from constructs import Construct
 
+from wordpress_stack import WordpressStack
+from wordpress_test_stack import WordpressTestStack
+
 class CdkpipelineStack(cdk.Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        CodePipeline(self, "Pipeline", 
+        pipeline= CodePipeline(self, "Pipeline", 
                 pipeline_name="MyPipeline",
                 synth=ShellStep("Synth", 
                     input=CodePipelineSource.git_hub("mohammad-ahmadi-r/cdk-pipeline", "main"),
@@ -20,3 +23,7 @@ class CdkpipelineStack(cdk.Stack):
                 )
             )
 
+        test_stage= pipeline.add_stage(WordpresTestStack(self, "test"))
+        test_stage.add_post(ManualApprovalStep('approval'))
+
+        prod_stage= pipeline.add_stage(WordpressStack(self, "prod"))
